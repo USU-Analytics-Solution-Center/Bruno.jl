58934e53b1ef06668a9e4278cc0b34fd773194c5
Documenter.jl <documenter@juliadocs.github.io>
2022-08-22 14:39:46 +0000

    build based on 26edf7c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Bruno.jl","page":"Bruno.jl","title":"Bruno.jl","text":"","category":"section"},{"location":"","page":"Bruno.jl","title":"Bruno.jl","text":"Documentation for Bruno.jl","category":"page"}]
+}
