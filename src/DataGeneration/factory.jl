d002d5a13be4a4c2abeb5f4269cff6a7f9d5746c
Mitch Pound <mitchell.pound@usu.edu>
2022-09-12 15:39:23 -0600

    commit to begin merging

--- src/DataGeneration/factory.jl
+++ src/DataGeneration/factory.jl
@@ -1,18 +1,19 @@
-function factor(widget::Widget, bootstrap_method::TSBootMethod, nWidgets::Signed)
-    input = BootstrapInput{bootstrap_method}(;
+function factory(widget::Widget, bootstrap_method::TSBootMethod, nWidgets::Signed)
+    input = BootstrapInput{typeof(bootstrap_method)}(;
                                 input_data = widget.prices,
                                 n = length(widget.prices),
                                 block_size = opt_block_length(widget.prices, bootstrap_method),
-                                dt = widget.dt)
+                                dt = widget.time_delta)
 
     bs_data = getData(input, nWidgets)
     widget_ar = Array{Widget}
     for column in 1:nWidgets
-        push!(widget_ar, typeof(widget)(bs_data[!, column], 
+        x = typeof(widget)(bs_data[:, column], 
                             widget.name,
-                            widget.volatility,
-                            widget.time_delta))
+                            var(bs_data[:, column]),                            
+                            widget.time_delta)
+        # println(x)
+    return(x)
     end
-
-    return widget_ar
+    
 end
