9409b25d3820cc8a719eaf454d221e60a3cb5eda
spencer <Soclemens@gmail.com>
2022-09-15 14:14:39 -0600

    Correctly making stock widget and sending to julia factory

--- python_wrapper/test.py
+++ python_wrapper/test.py
@@ -2,8 +2,6 @@
 # Need to figure out how to dynamucally do this...
 # Maybe have an outer python function that the user calls
 # and it handels the julia download/ seperate function calls
-import julia
-julia.install()
 
 from time import process_time
 from julia import Main
@@ -11,6 +9,13 @@ import pandas as pd
 
 print("Loading Julia")
 js_start = process_time()
-Main.include("src/Instruments/widgets.jl")
+Main.include("src/Instruments/Instruments.jl")
+Main.include("src/DataGeneration/DataGeneration.jl")
 js_loaded = process_time()
 print("loaded Julia in ", js_loaded - js_start)
+
+date_set = pd.read_csv("examples/AAPL.CSV")["Adj Close"].tolist()
+an_obj = Main.Instruments.Stock(date_set, "AAPL", -1, 1)
+list_of_widgets = Main.DataGeneration.factory(an_obj, Main.DataGeneration.Stationary(), 1)
+
+print(list_of_widgets)
