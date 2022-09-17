7bd8356dc6f38c804a4021c596e7b656e35f3007
Mitch Pound <mitchell.pound@usu.edu>
2022-09-16 18:02:06 -0600

    added Models module and moved models type system

--- src/Models/Models.jl
+++ src/Models/Models.jl
@@ -1,7 +1,13 @@
-# types for models to dispatch on in order to get values for FinancialInstruments
+module Models
+    
+using ..Instruments
+using ..DataGeneration
 
 abstract type Model end
 
 primitive type BlackScholes <: Model 8 end
 primitive type BinomialTree <: Model 8 end
 primitive type MonteCarlo <: Model 8 end
+
+
+end #module
