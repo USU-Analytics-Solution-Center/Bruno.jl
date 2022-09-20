ea787954d6fcaa7edb9b02ffdb4a89385e8d81f6
spencer clemens <soclemens@gmail.com>
2022-09-20 17:42:50 -0600

    adding docs

--- src/Models/pricingmodels.jl
+++ src/Models/pricingmodels.jl
@@ -1,9 +1,19 @@
 """
-## Description 
-The functions needed to run a binomial tree
-https://magnimetrics.com/understanding-the-binomial-option-pricing-model/
-"""
+price(fin_obj::EuroCallOption, pricing_model::Type{BinomialTree}, tree_depth, r, strike_price, delta)
 
+Computes the value of an European Call Option. 
+
+
+# Example
+```
+using Bruno
+
+ 
+a_stock = Stock([41], "example", .3)  # create a widget
+a_fin_inst = EuroCallOption(a_stock)  # create an Option
+x = price(a_fin_inst, BinomialTree, 3, .08, 40, 0)  # price the Option
+```
+"""
 function price(fin_obj::EuroCallOption, pricing_model::Type{BinomialTree}, tree_depth, r, strike_price, delta)
     """ 
     EURO OPTION
@@ -38,31 +48,31 @@ function price(fin_obj::AmericanCallOption, pricing_model::Type{BinomialTree}, t
     0
 end
 
-function b_tree(tree_depth, r, strike_price, time_to_mature, delta)
-    """ 
-    EURO OPTION
-    tree_depth = the depth of the tree
-    r = rate of return
-    sigma = volatility
-    strike_price = the strike price in dollars
-    time_to_mature = time to maturity in years (.5 == 1/2 year) || (1 == 1 year)
-    delta = intrest rate
-    """
-    S0 = 41  # Starting Price  replace me with widget last price
-    sigma = .3  # get sigma from widget price history
-    dt = time_to_mature / tree_depth
+# function b_tree(tree_depth, r, strike_price, time_to_mature, delta)
+#     """ 
+#     EURO OPTION
+#     tree_depth = the depth of the tree
+#     r = rate of return
+#     sigma = volatility
+#     strike_price = the strike price in dollars
+#     time_to_mature = time to maturity in years (.5 == 1/2 year) || (1 == 1 year)
+#     delta = intrest rate
+#     """
+#     S0 = 41  # Starting Price  replace me with widget last price
+#     sigma = .3  # get sigma from widget price history
+#     dt = time_to_mature / tree_depth
 
-    u = exp((r - delta) * dt + sigma * sqrt(dt))  # up movement 
-    d = exp((r - delta) * dt - sigma * sqrt(dt))  # down movement
-    p = (exp(r * dt) - d) / (u - d)  # risk neutral probability of an up move
+#     u = exp((r - delta) * dt + sigma * sqrt(dt))  # up movement 
+#     d = exp((r - delta) * dt - sigma * sqrt(dt))  # down movement
+#     p = (exp(r * dt) - d) / (u - d)  # risk neutral probability of an up move
     
-    c = 0
-    # value of the call is a weighted average of the values at each node multiplied by the corresponding probability value
-    for k in tree_depth:-1:0
-        p_star = (factorial(tree_depth) / (factorial(tree_depth - k) * factorial(k))) * p ^ k * (1 - p) ^ (tree_depth - k)
-        ST = S0 * u ^ k * d ^ (tree_depth - k)
-        c += max(ST - strike_price, 0) * p_star
-    end
+#     c = 0
+#     # value of the call is a weighted average of the values at each node multiplied by the corresponding probability value
+#     for k in tree_depth:-1:0
+#         p_star = (factorial(tree_depth) / (factorial(tree_depth - k) * factorial(k))) * p ^ k * (1 - p) ^ (tree_depth - k)
+#         ST = S0 * u ^ k * d ^ (tree_depth - k)
+#         c += max(ST - strike_price, 0) * p_star
+#     end
 
-    exp(-r * time_to_mature) * c
-end
+#     exp(-r * time_to_mature) * c
+# end
