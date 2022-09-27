afc6bdec2bfbdb599acaa63666849db24070556d
spencer clemens <soclemens@gmail.com>
2022-09-27 16:09:48 -0600

    added commoditty

--- profile/runprofile.jl
+++ profile/runprofile.jl
@@ -13,15 +13,19 @@ end
 
 function main()
     list_of_functions = collect_functions(Bruno)
-    deleteat!(list_of_functions, findall(x->x=="Bruno", list_of_functions))  # remove the Bruno tag as its doesnt need to be tested
-    df = DataFrame(functions=list_of_functions, time=[-1 for v in list_of_functions])  # Set up the df to have negative run times. IF a functions stays negative we know it wasnt tested
+
+    # Deletes things that dont need to be tested like "Bruno", "Widget
+    deleteat!(list_of_functions, findall(x->x=="Bruno", list_of_functions))
+    deleteat!(list_of_functions, findall(x->x=="Widget", list_of_functions))
     
+    # Set up
+    df = DataFrame(functions=list_of_functions, time=[-1 for v in list_of_functions])  # Set up the df to have negative run times. IF a functions stays negative we know it wasnt tested
     generic_arguments = Dict(:prices => [151.76, 150.77, 150.43, 152.74, 153.72, 156.90, 154.48, 150.70, 152.37, 155.31, 153.84], 
                                 :volatility => .05, 
                                 :name => "a_name")
-    known_functions = [profile_stock]  # <--- add the head of a function here after writing it
-    
+
     # Start calling the known functions
+    known_functions = [profile_stock, profile_commodity]  # <--- add the head of a function here after writing it
     results = Dict()
     for a_function in known_functions
         name, elapsed = a_function(generic_arguments)
@@ -35,10 +39,11 @@ Function tests below. Each function returns the name of the functions it
 is testing in the first postion and the time the profiler takes in the
 second.
 
-As a note make sure the function in question has a chance to go through
-the jit before being profiled. 
+As a note try to test the worst case. As an example if we give stock 
+all struct variables it wont have to calculate the var. 
 
 Functions calls written:
+    Stock
 
 Functions calls to be written:
     AbstractAmericanCall    
@@ -68,10 +73,8 @@ Functions calls to be written:
     Option                  
     PutOption               
     Stationary              
-    StationaryBootstrap     
-    Stock                   
-    TSBootMethod            
-    Widget                  
+    StationaryBootstrap                        
+    TSBootMethod                              
     b_tree                  
     data_gen_input          
     factory                 
@@ -83,9 +86,14 @@ Functions calls to be written:
 
 function profile_stock(kwargs)
     prices = kwargs[:prices]
-    stock = Stock(prices)
 
     timed = @benchmark Stock($prices);
-    # println(timed.TrialEstimate)
     return ("Stock", mean(timed).time)
+end
+
+function profile_commodity(kwargs)
+    prices = kwargs[:prices]
+
+    timed = @benchmark Commodity($prices);
+    return ("Commodity", mean(timed).time)
 end
