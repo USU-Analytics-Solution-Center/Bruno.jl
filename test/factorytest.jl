44651235bf159508664778b381da18c556da8abb
spencer clemens <soclemens@gmail.com>
2022-09-26 18:56:30 -0600

    added fin instrs factory test

--- test/factorytest.jl
+++ test/factorytest.jl
@@ -21,10 +21,21 @@ import InteractiveUtils
             fields = [p for p in fieldnames(typeof(list_of_widgets[1]))]  # get the first item from list of widgets as all others will be indeticale
             iter = Dict(fields .=> getfield.(Ref(list_of_widgets[1]), fields))
             @test length(findall(Base.isempty, iter)) == 0  # Test all fields in each widget have been filled in
+
+            @test length(list_of_widgets) == length(unique(list_of_widgets))  # check each new widget is unique
         end
     end
 end
 
 @testset "FinancialInstrument factory" begin
+    """
+    We need to figure out what we are exporting and how gather all known
+    fin_instruments to replicate widget factory. For now I'm just
+    implementing a single example for bair bones testing.
+    """
+    a_stock = Stock([100, 103, 105, 110, 113, 105, 98, 94, 110])
+    a_fin_inst = EuroCallOption(a_stock, 105)
+    list_of_fins = factory(a_fin_inst, Stationary, 5)
 
+    @test length(list_of_fins) == length(unique(list_of_fins))
 end
