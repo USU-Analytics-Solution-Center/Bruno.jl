f52d331523926fe21d5f2043c0fa36fd2ad7d2c1
Mitch Pound <mitchell.pound@usu.edu>
2022-09-29 10:04:04 -0600

    Added documentation for Stock and Commodity

--- src/Instruments/widgets.jl
+++ src/Instruments/widgets.jl
@@ -21,6 +21,11 @@ a_widget = Widget(prices=[1, 2, 3, 4, 5], name ="Example", volatility =.3)
 abstract type Widget end
 
 # ------ Stocks ------
+""" 
+    Stock <: Widget
+
+Widget subtype. Used as a base or root asset for FinancialInstrument
+"""
 struct Stock <: Widget 
     prices::Array{AbstractFloat}
     name::String
@@ -31,6 +36,9 @@ struct Stock <: Widget
         # allows single price input through kwargs (and ordered arguments)
         if typeof(prices) <: Real 
             prices >= 0 ? prices = [prices] : error("Single price point must be non-negative")
+            volatility == nothing ? 
+                error("When using single value input for prices must specify volatility") :
+                nothing
         end
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
         volatility >= 0 ? nothing : error("volatility must be non negative")
@@ -41,6 +49,9 @@ struct Stock <: Widget
     function Stock(prices, name = "", volatility = get_volatility(prices))  
         if typeof(prices) <: Real 
             prices >= 0 ? prices = [prices] : error("Single price point must be non-negative")
+            volatility == nothing ? 
+                error("When using single value input for prices must specify volatility") :
+                nothing
         end
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
         volatility >= 0 ? nothing : error("volatility must be non negative")
@@ -49,12 +60,45 @@ struct Stock <: Widget
 end
 
 # outer constructor to make a stock with a (static) single price
+"""
+    Stock(prices, name, volatility)
+    Stock(;kwargs)
+    Stock(price; kwargs)
+
+Construct a Stock type to use as a base asset for FinancialInstrument.
+
+## Arguments
+- `prices`:Historical prices (input as a 1-D array) or the current price input as a number
+`<: Real`
+- `name::String`: Name of the stock or stock ticker symbol. Default "".
+- `volatility`: Return volatility, measured in the standard deviation of continuous returns.
+Defaults to using `get_volatility()` on the input `prices` array. Note: if a single number 
+is given for `prices` volatility must be given.
+
+## Examples
+```jldoctest
+julia> Stock([1,2,3,4,5], "Test", .05)
+Stock(AbstractFloat[1.0, 2.0, 3.0, 4.0, 5.0], "Test", 0.05)
+
+julia> kwargs = Dict(prices=>[1,2,3,4,5], name=>"Test", volatility=>.05)
+julia> Stock(;kwargs...)
+Stock(AbstractFloat[1.0, 2.0, 3.0, 4.0, 5.0], "Test", 0.05)
+
+julia> Stock(40; volatility=.05)
+Stock(AbstractFloat[40.0], "", 0.05)
+```
+"""
 function Stock(price::Real; name = "", volatility)
     prices = [price]
     Stock(;prices = prices, name = name , volatility = volatility)
 end
 
 # ------ Commodities ------
+""" 
+    Commodity <: Widget
+
+Widget subtype. Used as a base or root asset for FinancialInstrument
+"""
 struct Commodity <: Widget
     prices::Array{AbstractFloat}
     name::String
@@ -62,6 +106,13 @@ struct Commodity <: Widget
 
     # constructor for kwargs
     function Commodity(; prices, name = "", volatility = get_volatility(prices), _...)
+        # allows for single number input for prices
+        if typeof(prices) <: Real 
+            prices >= 0 ? prices = [prices] : error("Single price point must be non-negative")
+            volatility == nothing ? 
+                error("When using single value input for prices must specify volatility") :
+                nothing
+        end
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
         volatility >= 0 ? nothing : error("volatility must be non negative")
         new(prices, name, volatility)
@@ -69,6 +120,12 @@ struct Commodity <: Widget
 
     # constructor for ordered argumentes 
     function Commodity(prices, name = "", volatility = get_volatility(prices))  
+        if typeof(prices) <: Real 
+            prices >= 0 ? prices = [prices] : error("Single price point must be non-negative")
+            volatility == nothing ? 
+                error("When using single value input for prices must specify volatility") :
+                nothing
+        end
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
         volatility >= 0 ? nothing : error("volatility must be non negative")
         new(prices, name, volatility)
@@ -76,6 +133,34 @@ struct Commodity <: Widget
 end
 
 # outer constructor to make a Commodity with a (static) single price
+"""
+    Commodity(prices, name, volatility)
+    Commodity(;kwargs)
+    Commodity(price; kwargs)
+
+Construct a Commodity type to use as a base asset for FinancialInstrument.
+
+## Arguments
+- `prices`:Historical prices (input as a 1-D array) or the current price input as a number
+`<: Real`
+- `name::String`: Name of the commodity or commodity ticker symbol. Default "".
+- `volatility`: Return volatility, measured in the standard deviation of continuous returns.
+Defaults to using `get_volatility()` on the input `prices` array. Note: if a single number 
+is given for `prices` volatility must be given.
+
+## Examples
+```jldoctest
+julia> Commodity([1,2,3,4,5], "Test", .05)
+Commodity(AbstractFloat[1.0, 2.0, 3.0, 4.0, 5.0], "Test", 0.05)
+
+julia> kwargs = Dict(prices=>[1,2,3,4,5], name=>"Test", volatility=>.05)
+julia> Commodity(;kwargs...)
+Commodity(AbstractFloat[1.0, 2.0, 3.0, 4.0, 5.0], "Test", 0.05)
+
+julia> Commodity(40; volatility=.05)
+Commodity(AbstractFloat[40.0], "", 0.05)
+```
+"""
 function Commodity(price::AbstractFloat; name = "", volatility)
     prices = [price]
     Commodity(;prices = prices, name = name , volatility = volatility)
@@ -91,12 +176,14 @@ struct Bond <: Widget
     # constructor for kwargs
     function Bond(; prices, name="", time_mat=1, coupon_rate=.03, _...)
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
+        time_mat > 0 ? nothing : error("time_mat must be positive")
         new(prices, name, time_mat, coupon_rate)
     end
 
     # constructor for ordered argumentes 
     function Bond(prices, name="", time_mat=1, coupon_rate=.03)  
         size(prices)[1] > 0 ? nothing : error("Prices cannot be an empty vector")
+        time_mat > 0 ? nothing : error("time_mat must be positive")
         new(prices, name, time_mat, coupon_rate)
     end
 end
@@ -108,8 +195,13 @@ function Bond(price::AbstractFloat; name="", time_mat=1, coupon_rate=.03)
 end
 
 # Helpers 
+"""
+    get_volatility(prices)
 
+Finds the standard deviation of continuous returns for an array of prices
+"""
 function get_volatility(prices) 
+    length(prices) > 1 ? nothing : return nothing
     returns = [((prices[i+1] - prices[i]) / prices[i]) + 1 for i in 1:(length(prices) - 1)]
     cont_return = log.(returns)
     std(cont_return) 
