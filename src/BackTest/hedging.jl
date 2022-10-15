44887f973bd223cc8997860ebec750c79a1fea11
spencer clemens <soclemens@gmail.com>
2022-10-15 12:59:16 -0600

    hedging at the most basic level works now. We just now need to add intrest and write new strategy functions

--- src/BackTest/hedging.jl
+++ src/BackTest/hedging.jl
@@ -1,7 +1,9 @@
+using Distributions
+
 primitive type DeltaHedge 8 end
 primitive type Expiry <: Model 8 end
 
-function get_returns(obj, sign, future_prices, strategy_mode, pricing_mode, n_timesteps)
+function get_returns(obj, sign, future_prices, strategy_mode, pricing_mode)
     # + means going long (buying) - means going short (selling)
     if typeof(obj) <: FinancialInstrument  # Check to see if a given FinancialInstrument has been pre-loaded
         if obj.values_library == Dict{String, Dict{String, AbstractFloat}}()
@@ -12,7 +14,7 @@ function get_returns(obj, sign, future_prices, strategy_mode, pricing_mode, n_ti
     # buy or sell the obj depnding on the sign
     bank = -sign(Models.price!(obj, pricing_mode))
 
-    bank = simulate(obj, future_prices, strategy_mode, pricing_mode, bank, n_timesteps)
+    bank = simulate(obj, future_prices, strategy_mode, pricing_mode, bank)
 
     # unwind position - sell what you have left buy what you short sold
     bank += sign(Models.price!(obj, Expiry))
@@ -21,22 +23,20 @@ function get_returns(obj, sign, future_prices, strategy_mode, pricing_mode, n_ti
 end
 
 
-function simulate(fin_obj::Option, future_prices, strategy_mode, pricing_mode, trading_profit, n_timesteps)
+function simulate(fin_obj::Option, future_prices, strategy_mode, pricing_mode, trading_profit)
     # how much money we get/ loose from hedging the position at each time step
     # assums we are also closing any hedging positions at the end
     # for buying an option
-
     prev_hedging_asset_count = 0
-    for i in 1:n_timesteps
+    for i in 1:lastindex(future_prices)
         # gives us the "optimal" holding ratio of fin_obj to underlying asset
         hedging_asset_count = strategy(fin_obj, strategy_mode)
-        # # "sells" and "buys" fin_obj and underlyign asset to get to the holding_ratio
+
         # trading_profit += interest(stuff...)
-        # trading_profit += rebalance(hedging_asset_count, prev_hedging_asset_count, fin_obj)
- 
-        # prev_hedging_asset_count = hedging_asset_count
-        # push!(fin_obj.widget.prices, popfirst!(future_prices))
-        # price!(fin_obj, pricing_mode)
+        trading_profit += (prev_hedging_asset_count - hedging_asset_count) * price!(fin_obj, pricing_mode)  # "sells" and "buys" fin_obj and underlyign asset to get to the holding_ratio
+        prev_hedging_asset_count = hedging_asset_count
+        push!(fin_obj.widget.prices, popfirst!(future_prices))
+        println(trading_profit)
     end
 
     # # at end of the time steps sell the hedging object
@@ -45,13 +45,17 @@ function simulate(fin_obj::Option, future_prices, strategy_mode, pricing_mode, t
     return trading_profit
 end
 
+function rebalance(hedging_asset_count, prev_hedging_asset_count, fin_obj)
+    return 
+end
+
 #-------strategies----------#
 function strategy(fin_obj, strategy_mode::Type{one_to_one})
     return 1
 end
 
 function strategy(fin_obj, strategy_mode::Type{RatioHedging})
-    return 1
+    rand(Uniform(0.4,0.6))
 end
 
 #-------Custome Price!----------#
@@ -68,9 +72,6 @@ function find_correlation_coeff(obj_a::Union{Stock, Commodity}, obj_b::Union{Sto
     """
 Pearson correlation
 """
-    # Find the returns
-    # obj_a_returns = [(obj_a.prices[i + 1] - obj_a.prices[i]) / obj_a.prices[i] for i in 1:(lastindex(obj_a.prices) - 1)]
-    # obj_b_returns = [(obj_b.prices[i + 1] - obj_b.prices[i]) / obj_b.prices[i] for i in 1:(lastindex(obj_b.prices) - 1)]
     a_average = sum(obj_a.prices) / lastindex(obj_a.prices)
     b_average = sum(obj_b.prices) / lastindex(obj_b.prices)
     cov = sum((obj_a.prices .- a_average) .* (obj_b.prices .- b_average)) / sqrt(sum((obj_a.prices .- a_average) .^ 2)  * sum((obj_b.prices .- b_average) .^ 2))
@@ -79,4 +80,8 @@ end
 
 function find_correlation_coeff(obj_a::Union{Stock, Commodity}, obj_b::Option)
     find_correlation_coeff(obj_a, obj_b.widget)
+end
+
+function find_correlation_coeff(obj_a::Option, obj_b::Union{Stock, Commodity})
+    find_correlation_coeff(obj_b, obj_a.widget)
 end
