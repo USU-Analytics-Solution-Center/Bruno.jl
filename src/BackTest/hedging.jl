d2f7c01e940dd4979bb64dd2f39c6f12519afc66
spencer clemens <soclemens@gmail.com>
2022-10-17 17:43:00 -0600

    added objupdate for widget to handle widgets

--- src/BackTest/hedging.jl
+++ src/BackTest/hedging.jl
@@ -1,10 +1,13 @@
 function strategy_returns(obj, pricing_model, strategy_type, future_prices, n_timesteps, timesteps_per_period, 
                             cash_injection=0.0, fin_obj_count=0.0, widget_count=0.0,pay_int_rate=0, hold_return_int_rate=0; kwargs...)
+    # Make some checks
+    length(future_prices) < n_timesteps ? error("Not enough future prices to accomidate the given amount of time steps.") : nothing
+    # TODO Add A check for     
+    
     # Set up the function  
     future_prices = deepcopy(future_prices)  # we do deep copies so the objects out of scope arent stomped on
     obj = deepcopy(obj)
-    length(future_prices) < n_timesteps ? error("Not enough future prices to accomidate the given amount of time steps.") : nothing
-    
+
     # set up holdings dictionary. Holdings is the active holdings of the program while ts_holdings produces a history
     holdings = Dict("cash" => cash_injection, "fin_obj_count" => fin_obj_count, "widget_count" => widget_count)
     ts_holdings = Dict("cash" => [cash_injection], "fin_obj_count" => [fin_obj_count], "widget_count" => [widget_count])
@@ -29,8 +32,6 @@ function strategy_returns(obj, pricing_model, strategy_type, future_prices, n_ti
         popfirst!(get_prices(obj))  # remove the most stale price
 
         obj = update_obj(obj, strategy_type, pricing_model, n_timesteps, timesteps_per_period, step)
-        println("object maturity \t", obj.maturity)
-        println("time step \t", step, "\n")
     end
 
     # unwind the postions
@@ -139,6 +140,11 @@ function update_obj(obj::Option, _::Type{<:Hedging}, pricing_model, _, timesteps
                          strike_price = obj.strike_price)
    Models.price!(new_obj, pricing_model)
 
+   return new_obj
+end
+
+function update_obj(obj::Widget, _::Type{<:Hedging}, _, _, _, _)
+    new_obj = deepcopy(obj)
    return new_obj
 end
 
